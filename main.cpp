05140c8482a1887d4a5e83fb6d6722d245f795c8
António Azevedo <antazevedo2003@gmail.com>
2023-05-30 13:58:29 +0100

    added execution time clock

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include "src/Scraper.h"
+#include <chrono>
 
 using namespace std;
 
 int main(){
     Graph gh;
 
-    Scraper::scrape_graph("../src/data/real/graph3/nodes.csv", gh, Scraper::real);
+    auto start = chrono::high_resolution_clock::now();
+
+    Scraper::scrape_graph("../src/data/real/graph1/nodes.csv", gh, Scraper::real);
+
+    auto finish = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = finish - start;
+
+    cout << "Elapsed time: " << elapsed.count() << " s\n";
     return 0;
 }
