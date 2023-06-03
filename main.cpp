d5d65f19a6d52906159603b5b1edee949e4f372d
António Azevedo <antazevedo2003@gmail.com>
2023-06-03 15:57:52 +0100

    fixed another small menu bug

--- main.cpp
+++ main.cpp
@@ -3,12 +3,8 @@
 using namespace std;
 
 int main(){
-    auto start = chrono::high_resolution_clock::now();
+
     Menu menu = Menu();
-
-    auto finish = chrono::high_resolution_clock::now();
-    chrono::duration<double> elapsed = finish - start;
-    cout << "Elapsed time: " << elapsed.count() << " s\n";
-
+    
     return 0;
 }
